4bf46d1cc0fdc39e973dcae5b78a30e66970fd15
atomisu0312 <koma4024@gmail.com>
2025-07-27 10:38:48 +0900

    airflow ver2 to ver3 in chapter02

--- chapter02/dags/download_rocket_launches.py
+++ chapter02/dags/download_rocket_launches.py
@@ -1,7 +1,7 @@
 import json
 import pathlib
+from datetime import datetime, timedelta
 
-import airflow.utils.dates
 import requests
 import requests.exceptions as requests_exceptions
 from airflow import DAG
@@ -11,30 +11,30 @@ from airflow.operators.python import PythonOperator
 dag = DAG(
     dag_id="download_rocket_launches",
     description="Download rocket pictures of recently launched rockets.",
-    start_date=airflow.utils.dates.days_ago(14),
-    schedule_interval="@daily",
+    start_date=datetime.now() - timedelta(days=14),
+    schedule="@daily",
 )
 
 download_launches = BashOperator(
     task_id="download_launches",
-    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",  # noqa: E501
+    bash_command="curl -f -o /opt/airflow/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming' || echo 'Failed to download launches'",
     dag=dag,
 )
 
 
 def _get_pictures():
     # Ensure directory exists
-    pathlib.Path("/tmp/images").mkdir(parents=True, exist_ok=True)
+    pathlib.Path("/opt/airflow/images").mkdir(parents=True, exist_ok=True)
 
     # Download all pictures in launches.json
-    with open("/tmp/launches.json") as f:
+    with open("/opt/airflow/launches.json") as f:
         launches = json.load(f)
         image_urls = [launch["image"] for launch in launches["results"]]
         for image_url in image_urls:
             try:
                 response = requests.get(image_url)
                 image_filename = image_url.split("/")[-1]
-                target_file = f"/tmp/images/{image_filename}"
+                target_file = f"/opt/airflow/images/{image_filename}"
                 with open(target_file, "wb") as f:
                     f.write(response.content)
                 print(f"Downloaded {image_url} to {target_file}")
@@ -50,7 +50,7 @@ get_pictures = PythonOperator(
 
 notify = BashOperator(
     task_id="notify",
-    bash_command='echo "There are now $(ls /tmp/images/ | wc -l) images."',
+    bash_command='echo "There are now $(ls /opt/airflow/images/ | wc -l) images."',
     dag=dag,
 )
 
